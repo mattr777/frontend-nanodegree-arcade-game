8d40880932f74e4bbdb72119854f225a348fbc70
Matt Richards <bitbucket@richards777.com>
2015-05-29 01:27:07 -0600

    Some refactoring done.

--- README.md
+++ README.md
@@ -1,19 +1,18 @@
-frontend-nanodegree-arcade-game
-===============================
+# frontend-nanodegree-arcade-game
 
 Game starts automatically.  To restart the game, refresh the browser.
 
-# Object of the Game
+## Object of the Game
 The object of the game is to get your character to cross from the bottom of the screen
 to the top of the screen without colliding with anything.
 
-# How to Move
+## How to Move
 You use the arrow keys on the keyboard to move.  You are not allowed to move your character
 off the game board.  Attempts at illegal moves are ignored. Once you reach the top row, press
 the up arrow to get credit for crossing and reset your character to the starting row for another
 try.
 
-# Collisions
+## Collisions
 If your character collides with another entity, it will be reset to the starting position and
 no points are scored.
 
--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
 // TODO: add sound
 // TODO: add score board
 // TODO: derive enemies and players from Entity common class, put all entities in an entity array?
-// TODO: randomize location of bug creation and rates
 // TODO: at advanced levels, bugs in some rows go in reverse
 // TODO: allow users to specify png files, add their name, two person game
 // TODO: have a timer for a time limit, show game over
@@ -10,23 +9,55 @@
 // TODO: don't allow player to just sit in initial spot or one spot for too long
 // TODO: add a demo mode
 
-// Enemies our player must avoid
-var Enemy = function(startingRow, rate) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
 
+/**
+ * Enemies our player must avoid
+ * @param startingRow
+ * @param rate
+ * @constructor
+ */
+var Enemy = function(startingRow, rate) {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
-    var c = document.getElementsByTagName('canvas');
+    // TODO: perhaps delete the following line
+    //var c = document.getElementsByTagName('canvas');
+    this.setRow(startingRow);
+    this.setRate(rate);
     this.x = -101;
-    this.y = 225 - (startingRow - 2) * 83;
-    this.row = startingRow;
-    this.rate = rate;
-}
+};
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
+/**
+ * Update the enemy's row with the given value
+ * @param row
+ */
+Enemy.prototype.setRow = function (row) {
+    if (row < 2) {
+        row = 2;
+    } else if (row > 4) {
+        row = 4;
+    }
+    this.row = row;
+    this.y = 225 - (this.row - 2) * 83;
+};
+
+/**
+ * Update the enemy's rate with the given value.
+ * @param rate
+ */
+Enemy.prototype.setRate = function (rate) {
+    if (rate < 20) {
+        rate = 20;
+    } else if (rate > 60) {
+        rate = 60;
+    }
+    this.rate = rate;
+};
+
+/**
+ * Update the enemy's position, required method for game
+ * @param dt a time delta between ticks
+ */
 Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
@@ -36,17 +67,20 @@ Enemy.prototype.update = function(dt) {
     } else {
         this.x = -101;
         this.rate = Math.random() * 40 + 20;
+        this.setRow(Math.min(Math.floor(Math.random() * 3) + 2, 4));
     }
-}
+};
 
-// Draw the enemy on the screen, required method for game
+/**
+ * Draw the enemy on the screen, required method for game
+ */
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
+};
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+/**
+ * This class requires an update(), render() and a handleInput() method.
+ */
 var Player = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
@@ -55,22 +89,24 @@ var Player = function() {
     // a helper we've provided to easily load images
     this.sprite = 'images/char-boy.png';
 
-    var c = document.getElementsByTagName('canvas');
+    //var c = document.getElementsByTagName('canvas');
     this.x = 202;
     this.y = 403;
     this.row = 0;
-}
+};
 
-// Update the player's position, required method for game
-// Parameter: dt, a time delta between ticks
+/**
+ * Update the player's position, required method for game
+ * @param dt a time delta between ticks
+ */
 Player.prototype.update = function(dt) {
     // TODO: at advanced levels (drunk?) we could mess with the player's position
     if ((this.row > 1) || (this.row < 5))
     {
-        // possibility of a collision
-        for (var e in allEnemies) {
-            if (allEnemies[e].row === this.row) {
-                if (Math.abs(allEnemies[e].x - this.x) < 75) {
+        // we are in a row where enemy's travel, possibility of a collision
+        for (var i = 0, n = allEnemies.length; i < n; i++) {
+            if (allEnemies[i].row === this.row) {
+                if (Math.abs(allEnemies[i].x - this.x) < 75) {
                     // collision
                     this.x = 202;
                     this.y = 403;
@@ -80,47 +116,68 @@ Player.prototype.update = function(dt) {
         }
 
     }
-}
+};
 
-// Draw the player on the screen, required method for game
+/**
+ * Draw the player on the screen, required method for game
+ */
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
+};
 
-// Handle input from the keyboard
+Player.prototype.moveLeft = function () {
+    if (this.x > 100) {
+        this.x -= 101;
+    }
+};
+
+Player.prototype.moveUp = function() {
+    if (this.y > 70) {
+        this.y -= 83;
+        this.row++;
+    } else {
+        // TODO: score points, you made it across
+        this.x = 202;
+        this.y = 403;
+        this.row = 0;
+    }
+};
+
+Player.prototype.moveRight = function () {
+    if (this.x < 304) {
+        this.x += 101;
+    }
+};
+
+Player.prototype.moveDown = function () {
+    if (this.y < 403) {
+        this.y += 83;
+        this.row--;
+    }
+};
+
+/**
+ * Handle input from the keyboard
+ * @param inputKey
+ */
 Player.prototype.handleInput = function(inputKey) {
     // TODO: remove hard-coded step sizes
     if (inputKey === 'left') {
-        if (this.x > 100) {
-            this.x -= 101;
-        }
+        this.moveLeft();
     } else if (inputKey === 'up') {
-        if (this.y > 70) {
-            this.y -= 83;
-            this.row++;
-        } else {
-            // TODO: score points, you made it across
-            this.x = 202;
-            this.y = 403;
-            this.row = 0;
-        }
+        this.moveUp();
     } else if (inputKey === 'right') {
-        if (this.x < 304) {
-            this.x += 101;
-        }
+        this.moveRight();
     } else if (inputKey === 'down') {
-        if (this.y < 403) {
-            this.y += 83;
-            this.row--;
-        }
+        this.moveDown();
     } else {
         // TODO: use character to show confusion
     }
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
+};
 
 
-// Now instantiate your objects.
+// Now instantiate objects.
 // Place all enemy objects in an array called allEnemies
 var allEnemies = [
     new Enemy(2, Math.random() * 40 + 20),
@@ -131,8 +188,10 @@ var allEnemies = [
 // Place the player object in a variable called player
 var player = new Player();
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+/**
+ * This listens for key presses and sends the keys to the
+ * Player.handleInput() method.
+ */
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
